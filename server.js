10a1dc4bd7455f206d51611e2ceff4a48f7d5567
Cameron LaVallie <camlavallie@gmail.com>
2019-09-27 09:34:44 -0500

    First Commit

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ app.use('/api/posts', require('./routes/api/posts'));
 app.use(helmet());
 app.use(morgan('tiny'));
 require('./startup/prod')(app);
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
