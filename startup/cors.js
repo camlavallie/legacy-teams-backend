09b47034d01d52f69bcb431b4406f5ca23197745
Cameron LaVallie <camlavallie@gmail.com>
2019-10-02 11:47:12 -0500

    make it better

--- startup/cors.js
+++ startup/cors.js
@@ -1,18 +1,5 @@
 const cors = require("cors");
 
 module.exports = function(app) {
-  // Set up a whitelist and check against it:
-  var whitelist = ['http://www.legacyteams.net']
-  var corsOptions = {
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
-    }
-  }
-
-  // Then pass them to cors:
-  app.use(cors(corsOptions));
+  app.use(cors());
 };
