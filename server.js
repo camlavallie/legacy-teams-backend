26da4a7b3c9bf930b6fdae378a9b0b80221aad36
Cameron LaVallie <camlavallie@gmail.com>
2019-10-03 15:12:31 -0500

    make it better

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json({
 }));
 // app.get('/', (req, res) => res.send('API Running'));
 // Set up a whitelist and check against it:
-var whitelist = ['http://www.legacyteams.net', 'https://legacy-teams.herokuapp.com']
+var whitelist = ['http://www.legacyteams.net', 'https://legacy-teams.herokuapp.com', 'http://localhost:3000']
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
@@ -30,10 +30,17 @@ var corsOptions = {
 app.use(cors(corsOptions));
 
 // Define Routes 
-app.use('/api/users', cors(corsOptions), require('./routes/api/users'));
-app.use('/api/auth', cors(corsOptions), require('./routes/api/auth'));
-app.use('/api/profile', cors(corsOptions), require('./routes/api/profile'));
-app.use('/api/posts', cors(corsOptions), require('./routes/api/posts'));
+app.use('/api/users', require('./routes/api/users'), function (req, res) {
+      res.setHeader('Access-Control-Allow-Origin', 'http://www.legacyteams.net');});
+app.use('/api/auth', require('./routes/api/auth'), function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://www.legacyteams.net');
+});
+app.use('/api/profile', require('./routes/api/profile'), function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://www.legacyteams.net');
+});
+app.use('/api/posts', require('./routes/api/posts'), function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://www.legacyteams.net');
+});
 app.use(helmet());
 app.use(morgan('tiny'));
 
