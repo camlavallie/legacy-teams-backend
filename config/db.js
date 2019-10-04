5a81c06fea9dd6594f0aab8496cd24ba09b7e1b7
Cameron LaVallie <camlavallie@gmail.com>
2019-10-04 10:33:55 -0500

    make it better

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const config = require('config');
 
-const db = config.get('db');
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
