ab0b465efaa787e0a73621cb66ecfe5e7abe605c
Cameron LaVallie <camlavallie@gmail.com>
2019-10-04 11:02:39 -0500

    make it better

--- server.js
+++ server.js
@@ -1,33 +1,31 @@
-const cors = require('cors');
 const express = require('express');
-const connectDB = require('./config/db')
-const app = express();
 const helmet = require('helmet');
 const morgan = require('morgan');
+const cors = require('cors');
+const connectDB = require('./config/db')
 
+const app = express();
 
 // connect database
 connectDB();
 
 // Init Middleware
-app.use(express.json({
-  extended: false
-}));
+app.use(express.json({extended: false}));
 // app.get('/', (req, res) => res.send('API Running'));
 
 
 // Then pass them to cors:
-app.use(cors());
 
 // Define Routes 
+app.use(cors());
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
-app.use('/api/users', require('./routes/api/users', 'Access-Control-Allow-Headers', '*'));
-app.use('/api/auth', require('./routes/api/auth', 'Access-Control-Allow-Headers', '*'));
-app.use('/api/profile', require('./routes/api/profile', 'Access-Control-Allow-Headers', '*'));
-app.use('/api/posts', require('./routes/api/posts', 'Access-Control-Allow-Headers', '*'));
 app.use(helmet());
 app.use(morgan('tiny'));
-
+require('./startup/prod')(app);
 
 
 const PORT = process.env.PORT || 5000;
