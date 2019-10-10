377281b9d068d97b6de2aa7daa7b2c8549a40a77
Cameron LaVallie <camlavallie@gmail.com>
2019-10-10 10:07:28 -0500

    prod

--- config/db.js
+++ config/db.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
-
 const config = require('config');
-
 const db = config.get('db');
 
 const connectDB = async () => {
--- server.js
+++ server.js
@@ -14,13 +14,12 @@ app.use(express.json({
   extended: false
 }));
 
-// app.get('/', (req, res) => res.send('API Running'));
 
 
 // Then pass them to cors:
+// Define Routes 
 app.use(cors());
 app.options('*', cors());
-// Define Routes 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
@@ -30,6 +29,10 @@ app.use(morgan('tiny'));
 require('./startup/prod')(app);
 
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
+
+const PORT = process.env.PORT || 5000;
+const NODE_ENV = process.env.NODE_ENV || 5000; 
+
+app.listen(PORT, NODE_ENV, () => console.log(`Server started on port ${PORT} || ${NODE_ENV}`))
